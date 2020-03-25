01ddb2b89a2ba3c8f2a29a8d0eb6dac299a6025e
BartoszWysocki02 <62674875+BartoszWysocki02@users.noreply.github.com>
2020-03-25 22:19:48 +0000

    Add files via upload

---
+++ assessment 2/assessment 2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+
+
+namespace assessment_2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+          string phrase = Console.ReadLine();
+
+
+          string[] words = phrase.Split(' ');
+            //user input/array of the 2 files entered.
+            foreach (var word in words)
+            {
+                System.Console.WriteLine($"<{word}>");
+            }
+
+          string file1 = File.ReadAllText(words[0]);
+          string file2 = File.ReadAllText(words[1]);
+
+            //files are the same
+            if (file1 == file2)
+            {
+                Console.WriteLine("File1 and File2 are not different");
+            }
+            //if the files are not the same
+            else
+            {
+
+                Console.WriteLine("these files are different");
+            
+            }
+
+        }
+        
+    }
+}
+
+           
